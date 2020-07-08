79924fefa234fc2bd1d7490e743e27cb5d8fd8e7
IvanovDmtr <d.ivanov@protonelectrotex.com>
2020-07-08 12:54:20 +0300

    Добавил функцию установки полярности клоков SPI_SetSyncPolarity

--- Library/ZwSPI.c
+++ Library/ZwSPI.c
@@ -22,6 +22,21 @@ void SPI_Init(SPI_TypeDef* SPIx, uint32_t BR_bits, bool LSBFirst)
 }
 //-----------------------------------------------
 
+void SPI_SetSyncPolarity(SPI_TypeDef* SPIx, SPI_SyncPolarityEnum Polarity)
+{
+	switch (Polarity)
+	{
+	case FALL_Edge:
+		SPIx->CR1 &=~ SPI_CR1_CPOL;
+		break;
+
+	case RISE_Edge:
+		SPIx->CR1 |= SPI_CR1_CPOL;
+		break;
+	}
+}
+//-----------------------------------------------
+
 void SPI_WriteByte(SPI_TypeDef* SPIx, uint16_t Data)
 {
 	while (SPIx->SR & SPI_SR_BSY);
