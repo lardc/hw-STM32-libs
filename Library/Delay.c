31ca0e5450d9a0007ea632c88fff4c973d0026cd
asytyi <alexander.a.sytyi@gmail.com>
2020-07-27 20:50:55 +0300

    Переработаны функции задержки. Погрешность доведена до +0,5мкс

--- Library/Delay.c
+++ Library/Delay.c
@@ -1,18 +1,29 @@
 // Header
-//
 #include "Delay.h"
 
+// Includes
+#include "SysConfig.h"
+
 // Definitions
-#define ASM_NOP_x40 {__asm("nop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop");\
-					 __asm("nop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop");\
-					 __asm("nop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop");\
-					 __asm("nop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop");}
+#define SYSCLK_MHZ		(SYSCLK / 1000000)
 
 // Functions
 //
-void __attribute__((optimize("O0"))) DELAY_US(uint32_t Delay)
+void __attribute__((optimize("O0"))) __DELAY(uint32_t Delay)
 {
-	while (Delay--)
-		ASM_NOP_x40;
+	while (--Delay);
+}
+//-----------------------------------------------
+
+void __attribute__((optimize("O3"))) DELAY_US(uint32_t Delay)
+{
+	// Исполнение одного цикла __DELAY занимает 11 тактов
+	__DELAY(Delay * SYSCLK_MHZ / 11);
+}
+//-----------------------------------------------
+
+void __attribute__((optimize("O3"))) DELAY_MS(uint32_t Delay)
+{
+	DELAY_US(Delay * 1000);
 }
 //-----------------------------------------------
--- Library/Delay.h
+++ Library/Delay.h
@@ -2,11 +2,10 @@
 #define __DELAY_H
 
 // Include
-//
 #include "stdinc.h"
 
 // Functions
-//
 void DELAY_US(uint32_t Delay);
+void DELAY_MS(uint32_t Delay);
 
-#endif //__DELAY_H
+#endif // __DELAY_H
