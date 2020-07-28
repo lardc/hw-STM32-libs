d411932af47a2876af4d92b5f5e62b6a8fe3eb14
asytyi <alexander.a.sytyi@gmail.com>
2020-07-28 22:37:29 +0300

    Добавлено формирование даты в gen_git_info.cmd

--- gen_git_info.cmd
+++ gen_git_info.cmd
@@ -26,6 +26,12 @@ for /f %%i in ('git rev-parse HEAD') do set git_commit=%%i
 set commit_str=%commit_var% %brace%%git_commit%%brace%%dot_comma%
 echo %commit_str% >> %file_output%
 
+set date_var=static const char *git_date =
+
+for /f %%i in ('git log -1 "--pretty=%%cd" "--date=format:%%Y/%%m/%%dT%%H:%%M:%%S"') do set git_date=%%i
+set date_str=%date_var% %brace%%git_date%%brace%%dot_comma%
+echo %date_str% >> %file_output%
+
 (
 echo.
 echo #endif // __GIT_INFO_H
