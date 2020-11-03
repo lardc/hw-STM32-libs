4953b158d2c25bbb0ec13a3e5c7745c85df0624c
asytyi <alexander.a.sytyi@gmail.com>
2020-11-03 18:54:56 +0300

    Добавлена библиотека с операциями MIN, MAX, SIGN

---
+++ Library/Utils.h
@@ -0,0 +1,12 @@
+#ifndef __UTILS_H
+#define __UTILS_H
+
+// Include
+#include "stdinc.h"
+
+// Macro
+#define MAX(a, b) 			(((a) > (b)) ? (a) : (b))
+#define MIN(a, b) 			(((a) < (b)) ? (a) : (b))
+#define SIGN(a)				(((a) >= 0) ? 1 : -1)
+
+#endif // __UTILS_H
