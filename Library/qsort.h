ef412f58336f71d90e0a698efc0f7bf05808152d
IvanovDmtr <d.ivanov@protonelectrotex.com>
2021-01-26 11:57:45 +0300

    Бибилиотеки qsort есть в TrueStudio, поэтому эти удаляются

--- Library/qsort.h
+++
@@ -1,9 +0,0 @@
-#ifndef __QSORT_H
-#define __QSORT_H
-
-#include "stdinc.h"
-#include "ysizet.h"
-
-void qsort(void* base, size_t nmemb, size_t size, int (*compar)(const void*, const void*));
-
-#endif // __QSORT_H
