b82c45f105aa229acacd80114d874aafb443b281
d.ivanov <d.ivanov@proton-electrotex.com>
2021-04-27 15:47:56 +0300

    Добавлены функции для работы с ШИМ

--- Library/ZwTIM.c
+++ Library/ZwTIM.c
@@ -22,29 +22,6 @@ void TIM_OnePulseMode(TIM_TypeDef* TIMx, bool State)
 }
 //-----------------------------------------------
 
-void TIM15_16_17_PWM_CH1_Config(TIM_TypeDef* TIMx, float SystemClock, float Period)
-{
-	uint32_t PSC_TEMP = (uint32_t)(SystemClock / 1000000 * Period / 65536);
-	uint32_t ARR_TEMP = (uint32_t)((SystemClock / ((PSC_TEMP + 1) * 1000000)) * Period);
-
-	TIMx->PSC = PSC_TEMP;	//Значение предделителя
-	TIMx->ARR = ARR_TEMP;	//Значение до которого считает счетчик
-	TIMx->EGR |= TIM_EGR_UG;
-	TIMx->SR &= ~TIM_SR_UIF;
-
-	TIMx->CCR1 = 0;
-	TIMx->CCER |= TIM_CCER_CC1E | TIM_CCER_CC1P;
-	TIMx->BDTR |= TIM_BDTR_MOE;
-	TIMx->CCMR1 = TIM_CCMR1_OC1M_2 | TIM_CCMR1_OC1M_1;
-}
-//-----------------------------------------------
-
-void TIM15_16_17_PWM_CH1_SetValue(TIM_TypeDef* TIMx, uint16_t PWM_Value)
-{
-	TIMx->CCR1 = PWM_Value;
-}
-//-----------------------------------------------
-
 // Включение тактирования таймера
 void TIM_Clock_En(uint32_t TIM_x)
 {
@@ -127,5 +104,73 @@ bool TIM_StatusCheck(TIM_TypeDef* TIMx)
 void TIM_StatusClear(TIM_TypeDef* TIMx)
 {
 	TIMx->SR &= ~TIM_SR_UIF;
+}
+//-----------------------------------------------
+
+// PWM
+//
+void TIMx_PWM_SetValue(TIM_TypeDef* TIMx, uint32_t Channel, uint32_t Value)
+{
+	switch (Channel)
+	{
+		case TIMx_CHANNEL1:
+			TIMx->CCR1 = Value;
+			break;
+
+		case TIMx_CHANNEL2:
+			TIMx->CCR2 = Value;
+			break;
+
+		case TIMx_CHANNEL3:
+			TIMx->CCR3 = Value;
+			break;
+
+		case TIMx_CHANNEL4:
+			TIMx->CCR4 = Value;
+			break;
+	}
+}
+//-----------------------------------------------
+
+void TIMx_PWM_ConfigChannel(TIM_TypeDef* TIMx, uint32_t Channel)
+{
+	switch (Channel)
+	{
+		case TIMx_CHANNEL1:
+			TIMx->CCER |= TIM_CCER_CC1E;
+			TIMx->CCMR1 = TIM_CCMR1_OC1M_2 | TIM_CCMR1_OC1M_1;
+			break;
+
+		case TIMx_CHANNEL2:
+			TIMx->CCER |= TIM_CCER_CC2E;
+			TIMx->CCMR1 = TIM_CCMR1_OC2M_2 | TIM_CCMR1_OC2M_1;
+			break;
+
+		case TIMx_CHANNEL3:
+			TIMx->CCER |= TIM_CCER_CC3E;
+			TIMx->CCMR2 = TIM_CCMR2_OC3M_2 | TIM_CCMR2_OC3M_1;
+			break;
+
+		case TIMx_CHANNEL4:
+			TIMx->CCER |= TIM_CCER_CC4E;
+			TIMx->CCMR2 = TIM_CCMR2_OC4M_2 | TIM_CCMR2_OC4M_1;
+			break;
+	}
+
+	TIMx->BDTR |= TIM_BDTR_MOE;
+}
+//-----------------------------------------------
+
+void TIM15_16_17_PWM_CH1_Config(TIM_TypeDef* TIMx, float SystemClock, float Period)
+{
+	TIM_Config(TIMx, SystemClock, Period);
+	TIMx_PWM_SetValue(TIMx, 1, 0);
+	TIMx_PWM_ConfigChannel(TIMx, TIMx_CHANNEL1);
+}
+//-----------------------------------------------
+
+void TIM15_16_17_PWM_CH1_SetValue(TIM_TypeDef* TIMx, uint16_t PWM_Value)
+{
+	TIMx_PWM_SetValue(TIMx, TIMx_CHANNEL1, PWM_Value);
 }
 //-----------------------------------------------
--- Library/ZwTIM.h
+++ Library/ZwTIM.h
@@ -9,6 +9,11 @@
 #define MMS_ENABLE              TIM_CR2_MMS_0
 #define MMS_UPDATE              TIM_CR2_MMS_1
 #define DMAEN					TIM_DIER_UDE
+//
+#define TIMx_CHANNEL1			1
+#define TIMx_CHANNEL2			2
+#define TIMx_CHANNEL3			3
+#define TIMx_CHANNEL4			4
 
 void TIM_Config(TIM_TypeDef * TIMx, float SystemClock, float Period);
 void TIM_Interupt(TIM_TypeDef * TIMx, uint8_t Priority, bool EN);
@@ -23,5 +28,7 @@ void TIM_Clock_En(uint32_t TIM_x);
 void TIM15_16_17_PWM_CH1_Config(TIM_TypeDef* TIMx, float SystemClock, float Period);
 void TIM15_16_17_PWM_CH1_SetValue(TIM_TypeDef* TIMx, uint16_t PWM_Value);
 void TIM_OnePulseMode(TIM_TypeDef* TIMx, bool State);
+void TIMx_PWM_SetValue(TIM_TypeDef* TIMx, uint32_t Channel, uint32_t Value);
+void TIMx_PWM_ConfigChannel(TIM_TypeDef* TIMx, uint32_t Channel);
 
 #endif // __ZW_TIM_H
