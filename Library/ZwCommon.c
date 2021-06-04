96e2bd347f0cfc137078131f3b888890b85c2190
d.ivanov <d.ivanov@proton-electrotex.com>
2021-06-04 11:35:34 +0300

    Добавлена общая бибилиотка

---
+++ Library/ZwCommon.c
@@ -0,0 +1,14 @@
+﻿// Header
+#include "ZwCommon.h"
+
+// Includes
+#include "SysConfig.h"
+#include "ZwIWDG.h"
+
+// Functions
+//
+void COM_InterruptPrioritySet(IRQn_Type EXTI_IRQ, uint16_t Priority)
+{
+	NVIC_SetPriority(EXTI_IRQ, Priority);
+}
+//-----------------------------------------------
---
+++ Library/ZwCommon.h
@@ -0,0 +1,10 @@
+﻿#ifndef __ZWCOMMON_H
+#define __ZWCOMMON_H
+
+// Include
+#include "stdinc.h"
+
+// Functions
+void COM_InterruptPrioritySet(IRQn_Type EXTI_IRQ, uint16_t Priority);
+
+#endif // __ZWCOMMON_H
