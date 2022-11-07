bd45cb52a5a846899a18e272642476d96bf04268
d.ivanov <d.ivanov@proton-electrotex.com>
2022-11-07 14:30:42 +0300

    Изменены номера мэйлбоксов, чтобы не было наложений между мастером и слейвом

--- Interface/BCCIMaster.c
+++ Interface/BCCIMaster.c
@@ -14,27 +14,27 @@
 
 // Definitions
 //
-#define Master_MBOX_W_16			1
-#define Master_MBOX_W_16_A			2
-#define Master_MBOX_W_32			3
-#define Master_MBOX_W_32_A			4
-#define Master_MBOX_R_16			5
-#define Master_MBOX_R_16_A			6
-#define Master_MBOX_R_32			7
-#define Master_MBOX_R_32_A			8
-#define Master_MBOX_C				9
-#define Master_MBOX_C_A				10
-#define Master_MBOX_ERR_A			11
-#define Master_MBOX_RB_16			12
-#define Master_MBOX_RB_16_A			13
-#define Master_MBOX_R_F				20
-#define Master_MBOX_R_F_A			21
-#define Master_MBOX_W_F				22
-#define Master_MBOX_W_F_A			23
-#define Master_MBOX_RB_F			24
-#define Master_MBOX_RB_F_A			25
-#define Master_MBOX_RLIM_F			26
-#define Master_MBOX_RLIM_F_A		27
+#define Master_MBOX_W_16			0
+#define Master_MBOX_W_16_A			1
+#define Master_MBOX_W_32			2
+#define Master_MBOX_W_32_A			3
+#define Master_MBOX_R_16			4
+#define Master_MBOX_R_16_A			5
+#define Master_MBOX_R_32			6
+#define Master_MBOX_R_32_A			7
+#define Master_MBOX_C				8
+#define Master_MBOX_C_A				9
+#define Master_MBOX_ERR_A			10
+#define Master_MBOX_RB_16			11
+#define Master_MBOX_RB_16_A			12
+#define Master_MBOX_R_F				13
+#define Master_MBOX_R_F_A			14
+#define Master_MBOX_W_F				15
+#define Master_MBOX_W_F_A			16
+#define Master_MBOX_RB_F			17
+#define Master_MBOX_RB_F_A			18
+#define Master_MBOX_RLIM_F			19
+#define Master_MBOX_RLIM_F_A		20
 
 #define MIN(a, b) 					(((a) < (b)) ? (a) : (b))
 
