947a78d089c977b1abefcae952bfc1febdab4921
asytyi <alexander.a.sytyi@gmail.com>
2024-03-24 14:24:08 +0300

    Добавлена загрузка секции CCMRAM из флеш в RAM

--- System/System/startup_stm32f303xc.s
+++ System/System/startup_stm32f303xc.s
@@ -87,8 +87,28 @@ LoopCopyDataInit:
 	adds	r2, r0, r1
 	cmp	r2, r3
 	bcc	CopyDataInit
+
+/* Copy from flash to CCMRAM */
+	ldr r0, =_sccmram
+	ldr r1, =_eccmram
+	ldr r2, =_siccmram
+	movs r3, #0
+	b LoopCopyCcmInit
+
+CopyCcmInit:
+	ldr r4, [r2, r3]
+	str r4, [r0, r3]
+	adds r3, r3, #4
+
+LoopCopyCcmInit:
+	adds r4, r0, r3
+	cmp r4, r1
+	bcc CopyCcmInit
+/* End of copy to CCMRAM */
+
 	ldr	r2, =_sbss
 	b	LoopFillZerobss
+
 /* Zero fill the bss segment. */
 FillZerobss:
 	movs	r3, #0
