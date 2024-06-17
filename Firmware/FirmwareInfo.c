b953aba7001c072945d0ed8207a1d60ac60b3d4b
asytyi <alexander.a.sytyi@gmail.com>
2024-06-17 14:34:28 +0300

    Реализован универсальный алгоритм записи параметров + опция записи названия проекта

--- Firmware/FirmwareInfo.c
+++ Firmware/FirmwareInfo.c
@@ -1,63 +1,60 @@
 ﻿// Header
 #include "FirmwareInfo.h"
 
-// Include
-#include "DataTable.h"
-
-// Definitions
-#define COMMIT_LEN		7
-#define DATE_LEN		19
-
 // Includes
 #include "git_info.h"
+#include <string.h>
 
 // Forward functions
 void FWINF_AppendSymbol(pInt16U DataTable, Int16U Symbol, Int16U Counter);
+void FWINF_AppendString(pInt16U DataTable, const char *String, Int16U StringLen, pInt16U pCounter, Int16U MaxLength, bool IsLastString);
 
 // Functions
 //
 Int16U FWINF_Compose(pInt16U DataTable, Int16U MaxLength)
 {
-	Int16U BranchLen, i, counter = 0;
-	
-	// Compose commit info
-	for(i = 0; i < COMMIT_LEN && counter < MaxLength; i++)
-		FWINF_AppendSymbol(DataTable, git_commit[i], counter++);
-	if(counter < MaxLength)
-		FWINF_AppendSymbol(DataTable, ',', counter++);
-	
-	// Compose date info
-	for(i = 0; i < DATE_LEN && counter < MaxLength; i++)
-		FWINF_AppendSymbol(DataTable, git_date[i], counter++);
-	if(counter < MaxLength)
-		FWINF_AppendSymbol(DataTable, ',', counter++);
-	
-	// Compose date info
-	BranchLen = sizeof(git_branch) / sizeof(char) - 1;
-	for(i = 0; i < BranchLen && counter < MaxLength; i++)
-		FWINF_AppendSymbol(DataTable, git_branch[i], counter++);
-	
-	// Align counter to even
-	if(counter % 2 && counter < MaxLength)
-		FWINF_AppendSymbol(DataTable, ' ', counter++);
+	Int16U counter = 0;
+
+	FWINF_AppendString(DataTable, git_commit,	strlen(git_commit),	&counter, MaxLength, false);
+	FWINF_AppendString(DataTable, git_date,		strlen(git_date),	&counter, MaxLength, false);
+#ifdef GIT_INF_USE_PROJ
+	FWINF_AppendString(DataTable, git_branch,	strlen(git_branch),	&counter, MaxLength, false);
+	FWINF_AppendString(DataTable, git_proj,		strlen(git_proj),	&counter, MaxLength, true);
+#else
+	FWINF_AppendString(DataTable, git_branch,	strlen(git_branch),	&counter, MaxLength, true);
+#endif
 
 	return counter;
 }
 // ----------------------------------------
 
+void FWINF_AppendString(pInt16U DataTable, const char *String, Int16U StringLen, pInt16U pCounter, Int16U MaxLength, bool IsLastString)
+{
+	Int16U i;
+
+	// Добавление строки
+	for(i = 0; i < StringLen && *pCounter < MaxLength; i++)
+		FWINF_AppendSymbol(DataTable, String[i], (*pCounter)++);
+	if(*pCounter < MaxLength)
+	{
+		// Выравнивание размера данных
+		if(IsLastString)
+		{
+			if(*pCounter % 2)
+				FWINF_AppendSymbol(DataTable, ' ', (*pCounter)++);
+		}
+		// или добавление разделителя-запятой
+		else
+			FWINF_AppendSymbol(DataTable, ',', (*pCounter)++);
+	}
+}
+// ----------------------------------------
+
 void FWINF_AppendSymbol(pInt16U DataTable, Int16U Symbol, Int16U Counter)
 {
-#ifdef USE_FLOAT_DT
-	pFloat32 pDataTable = (pFloat32)DataTable;
-#else
-	pInt16U pDataTable = DataTable;
-#endif
 	if(Counter % 2)
-	{
-		Int16U sym = *(pDataTable + Counter / 2);
-		*(pDataTable + Counter / 2) = sym | (Symbol & 0xFF);
-	}
+		*(DataTable + Counter / 2) |= Symbol & 0xFF;
 	else
-		*(pDataTable + Counter / 2) = Symbol << 8;
+		*(DataTable + Counter / 2) = Symbol << 8;
 }
 // ----------------------------------------
