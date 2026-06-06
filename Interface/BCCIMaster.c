bec87fc05461e668af2f82a4e6551ed5f820f009
asytyi <alexander.a.sytyi@gmail.com>
2026-06-06 12:49:58 +0300

    Улучшена детализация при возникновении таймаута

--- Interface/BCCIMaster.c
+++ Interface/BCCIMaster.c
@@ -570,16 +570,14 @@ void BCCIM_SendBroadcastPing(pBCCIM_Interface Interface, pInt16U NodeArray, pInt
 */
 Int16U BCCIM_WaitResponse(pBCCIM_Interface Interface, Int16U Mailbox)
 {
-	Int64U timeout;
-	CANMessage message;
-
-	// Wait for response
-	timeout = Interface->TimeoutValueTicks + *(Interface->pTimerCounter);
-	while(*(Interface->pTimerCounter) < timeout)
+	Int64U StartTime = *(Interface->pTimerCounter);
+	Int64U Timeout = StartTime + Interface->TimeoutValueTicks;
+	while(*(Interface->pTimerCounter) < Timeout)
 	{
 		// In case of error
 		if (Interface->IOConfig->IO_IsMessageReceived(Master_MBOX_ERR_A, NULL))
 		{
+			CANMessage message;
 			Interface->IOConfig->IO_GetMessage(Master_MBOX_ERR_A, &message);
 			SavedErrorDetails = message.HIGH.WORD.WORD_1;
 			return message.HIGH.WORD.WORD_0;
@@ -593,7 +591,7 @@ Int16U BCCIM_WaitResponse(pBCCIM_Interface Interface, Int16U Mailbox)
 		IWDG_Refresh();
 	}
 
-	SavedErrorDetails = xCCI_TIMEOUT_TICKS;
+	SavedErrorDetails = *(Interface->pTimerCounter) - StartTime;
 	return ERR_TIMEOUT;
 }
 // ----------------------------------------
